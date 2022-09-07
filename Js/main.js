087b384e4b71aa6df78c55696f177230d5636c59
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-09-06 19:48:04 -0600

    Se prueba una manera para crear los espacios de trabajo, el día 6 de agosto. -Karen Valdés

---
+++ Js/main.js
@@ -0,0 +1,21 @@
+let elementos = ['','¿Qué es la bromatología?'];
+
+let root = document.querySelector("#root");
+
+function agregar_al_dom(){
+
+    elementos.forEach(function (contenido, indice){
+        let div = document.createElement("div");
+        div.innerHTML ='<h1 class = "espacio">'+contenido+'</h1>',
+        root.appendChild(div);
+    });
+}
+agregar_al_dom();
+
+function activar_letra(){
+
+    let las_cosas_del_dom = document.querySelectorAll(".espacio");
+    las_cosas_del_dom.forEach();
+}
+
+activar_letra();
