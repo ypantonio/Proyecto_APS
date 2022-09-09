13b2b09862ce18502f4c51a0a5554fb4c0be0c17
Yasmin Antonio <78930202+ypantonio@users.noreply.github.com>
2022-09-09 10:04:49 -0600

    Update home.js

--- Js/home.js
+++ Js/home.js
@@ -4,7 +4,7 @@
 export let back = document.createElement("div");
 back.innerHTML = 
 '<section class="vector" >'+
-    '<img src="img/Cocina 1.png" alt="" class="cocina">'+
+    '<img src="/img/Cocina 1.png" alt="" class="cocina">'+
     '<h1 class="titulo1"> Aprende a <br> seguir un <br> camino sano </h1>'+
 
 '</section>';
