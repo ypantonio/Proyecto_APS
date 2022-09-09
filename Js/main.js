ab846993c31600090915fb963b0c9d3aa3071328
ypantonio <anntonnio@DESKTOP-59A8TQN.localdomain>
2022-09-09 07:53:42 -0600

    elimina partes

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,4 @@
-/*MUESTRA PARTE DE YASMIN ANTONIO*/
-import {Crearheader} from "./componentes/header.js";
-Crearheader();
+
 
 /*MUESTRA PARTE DE KAREN VALDÉS*/
 import {AñadirInfo} from "./componentes/areaInformativa.js";
