fb1ff1871fa7c31df8dc9a24c7f422be3cd27e2a
Emmanuel <jefuentes@scl.edu.gt>
2022-09-14 13:26:10 -0600

    Se realizan configuraciones con el profe

--- Js/componentes/header.js
+++ Js/componentes/header.js
@@ -1,20 +1,21 @@
 /***NO CREAR MAS COSAS EN ESTE DOCUMENTO ***/
 /**ESTRUCTURA DE HAEDER* */
 export let header = document.createElement("div");
-header.innerHTML = 
-
-'<img src="./img/healthy-way-logo 1.png" class="logo">'+
-'<ul class="nav">'+
-    '<li> <a id="inicia" class="inicia" href="#"> Inicio </a>  </li>'+
-    '<li> <a class="carbo" href="#"> Olla alimentaria</a>  </li>'+
-    '<li> <a class="calor" href="#"> Estadísticas</a>  </li>'+
-    '<li> <a class="lipi" href="#"> Nutrientes</a>  </li>'+
-'</ul> ' +
-'<button class="boton">'+
-    '<div class="linea"></div>'+
-    '<div class="linea"></div>'+
-    '<div class="linea"></div>'+
-'</button>';
+header.innerHTML =
+`
+<img src="./img/healthy-way-logo 1.png" class="logo">
+<ul class="nav">
+    <li> <a id="inicia" class="inicia" href="#"> Inicio </a>  </li>
+    <li> <a class="carbo" href="#"> Olla alimentaria</a>  </li>
+    <li> <a class="calor" href="#"> Estadísticas</a>  </li>
+    <li> <a class="lipi" href="#"> Nutrientes</a>  </li>
+</ul>
+<button class="boton">
+    <div class="linea"></div>
+    <div class="linea"></div>
+    <div class="linea"></div>
+</button>
+`;
 
 export function Crearheader(){
     let div_header = document.querySelector("#header");
--- Js/main.js
+++ Js/main.js
@@ -3,12 +3,15 @@
 import {Crearheader} from "./componentes/header.js";
 Crearheader();
 
+/****CONECCIÓN DE LA HOME** */
+import { Crearfondo } from "./componentes/home.js";
+Crearfondo();
 
 /*MUESTRA PARTE DE KAREN VALDÉS*/
 import {AñadirInfo} from "./componentes/areaInformativa.js";
 AñadirInfo();
 
+/*MUESTRA PARTE DE BRYAN HERNADEZ*/
+import {CrearFooter} from "./componentes/footer.js";
+CrearFooter();
 
-/****CONECCIÓN DE LA HOME** */
-import { Crearfondo } from "./home.js";
-Crearfondo();
