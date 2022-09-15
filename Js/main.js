638123dd757c91437ce2fc64a3a446633a3f6899
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-09-15 15:38:59 -0600

    Se actualizaron unos archivos

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,3 @@
-/*MUESTRA PARTE DE YASMIN ANTONIO*/
-import {Crearheader} from "./componentes/header.js";
-Crearheader();
-
-/*MUESTRA PARTE DE KAREN VALDÉS*/
+/* PARTE DE KAREN VALDÉS*/
 import {AñadirInfo} from "./componentes/areaInformativa.js";
 AñadirInfo();
