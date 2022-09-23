f1606648c3865d90f64b5bb2115ba61e6fdbf264
bryan hernandez <107013710+123Aqwezxc123@users.noreply.github.com>
2022-09-23 08:58:23 -0600

    se agredo la parte de las proteínas

--- Js/componentes/infoProteinas.js
+++ Js/componentes/infoProteinas.js
@@ -5,6 +5,31 @@ export let pro = document.createElement("div");
 pro.innerHTML = 
 `
 <!--SECCIÓN BRYAN HERNANDEZ-->
+/***NO CREAR MAS COSAS EN ESTE DOCUMENTO ***/
+/**COMIENZO AREA INFORMATIVA DE PROTEINAS* */
+/**CONTIENE PARTE ASIGNADA DE BRYAN HERNANDEZ Y KAREN VALDES* */
+export let pro = document.createElement("div");
+pro.innerHTML = 
+`
+<div class="area">
+<div class="paraTexto">
+    <h1 class="titulo">¿Que son las proteínas?</h1>
+    <p class="parrafo">
+    Las proteínas son los componentes principales de los músculos y de otros sistemas que sean capaces de transformar la energía química de los alimentos en trabajo mecánico y también las encontramos formando parte de los sensores que nos permiten ver, oír y degustar.    </p>
+</div>
+<img class="Img1" src="./img/Proteinas_img1.png" alt="">
+</div>
+<!--SECCIÓN KAREN VALDES-->
+<section class="para_video">
+
+</section>
+
+`;
+
+export function CrearProteina(){
+    let div_proteina = document.querySelector("#Info_Pro");
+    div_proteina.appendChild(pro);
+}
 
 <!--SECCIÓN KAREN VALDES-->
 <section class="para_video">
