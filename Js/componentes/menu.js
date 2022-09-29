7e10910713973f7199faf1340881b4680b1f30a5
Ksvaldes <ksvaldes@sscl.edu.gt>
2022-09-29 13:20:27 -0600

    Se agrega base para empezar la parte de lípidos y parte asignada, el dia 29 de septiembre. -Karen Valdes

--- Js/componentes/menu.js
+++ Js/componentes/menu.js
@@ -70,3 +70,40 @@ export function Crearmenu2(){
     let div_menu2 = document.querySelector("#header");
     div_menu2.appendChild(menu2);
 }
+
+/************************************** */
+/**ESTRUCTURA DE HEADER LIPIDOS */
+export let menu3 = document.createElement("div");
+menu3.innerHTML = 
+`
+<!--ESTE ES EL HEADER-->
+
+<header id="para_header" class="para_header3">
+<div class="menu_principal">
+    <div class="para_img">
+        <img src="./img/healthy-way-logo 1.png" class="logo">
+    </div>
+    <div class="para_menu">
+        <ul class="nav">
+            <li> <a id="inicia" class="inicia a" href="./index.html"> Inicio </a>  </li>
+            <li> <a class="carbo a" href="./index.html#Olla-Alimentaria"> Olla alimentaria</a>  </li>
+            <li> <a class="calor a" href="./index.html#Estadisticas"> Estadísticas</a>  </li>
+            <li> <a class="lipi a" href="./index.html#Nutrientes"> Nutrientes</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="titulo_proteina">
+    <h2 class="tituloParaLi">Lípidos</h2>
+    <img src="./img/Lípidos 1.png" class="img_de_lipidos" alt="">
+</div>
+</header>
+
+<!--AQUÍ TERMINA EL HEADER-->
+
+`;
+
+export function Crearmenu3(){
+    let div_menu3 = document.querySelector("#header");
+    div_menu3.appendChild(menu3);
+}
